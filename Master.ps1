4fbcbef42656a87c1a995560acc051e2a2938795
Rhys Ferris <Rhys.j.ferris@gmail.com>
2019-03-08 11:06:32 -0800

    Update Master.ps1
    Added Basic print at beginning, Adjusted Params for Directory to be mandatory and to self validate

--- Master.ps1
+++ Master.ps1
@@ -1,11 +1,17 @@
 
+Write-Host "Install DC Step 1: Install-DC
+Install DC Step 2: DC-Promo -DomainName <domain.com> [-AddDC]
+Install Exchange Step 1: Install-ExchPreReq -Directory <C:\ExchFiles>
+Install Exchange Setp 2 (once per domain): Prepare-Exchange -Directory <C:\ExchFiles>
+Install Exchange Step 3: Install-Exchange -Directory <C:\ExchFiles>"
+
 function Install-DC {
     Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -IncludeAllSubFeature
 }
 
 function DC-Promo {
     param(
-        [string]$DomainName,
+        [string][Parameter(Mandatory=$true,Position=0)]$DomainName,
         [switch]$AddDC
     )
     if($AddDC){
@@ -19,30 +25,37 @@ function DC-Promo {
 }
 
 function Install-ExchPreReq{
-    #param ([string]$Directory)
-    #Push-Location $Directory #Change Dir to the given Directory
+    param (
+        [string][Parameter(Mandatory=$true, Position=0)][ValidateScript({test-path $_})]$Directory
+    )
+    Push-Location $Directory #Change Dir to the given Directory
+    Get-ChildItem | ForEach-Object { Unblock-File $_ }
     $myProcess = Start-Process "(1st) NDP471-KB4033342-x86-x64-AllOS-ENU.exe" -PassThru -ArgumentList "/q","/norestart"
         $myProcess | Wait-Process
-    write-host ".NET Framework Installed"
+    write-host ".NET Framework Installed" -ForegroundColor Green
     $myProcess = Start-Process "(2nd)vcredist_x64.exe" -PassThru -ArgumentList "/passive","/norestart"
         $myProcess | Wait-Process
-    Write-Host "Visiual C++ 2012 Installed"
+    Write-Host "Visiual C++ 2012 Installed" -ForegroundColor Green
     $myProcess = Start-Process "(3rd)vcredist_x64.exe" -PassThru -ArgumentList "/install","/passive","/norestart"
         $myProcess | Wait-Process
-    Write-Host "Visiual C++ 2013 Installed"
+    Write-Host "Visiual C++ 2013 Installed" -ForegroundColor Green
     $myProcess = Start-Process "(4th)UcmaRuntimeSetup.exe" -PassThru -ArgumentList "/passive","/norestart"
         $myProcess | Wait-Process
-    Write-Host "Unified Communications Managed API Installed"
+    Write-Host "Unified Communications Managed API Installed" -ForegroundColor Green
     #Pop-Location #return to previous directory
     sleep 4
     Install-WindowsFeature NET-Framework-45-Features, Server-Media-Foundation, RPC-over-HTTP-proxy, RSAT-Clustering, RSAT-Clustering-CmdInterface, RSAT-Clustering-Mgmt, RSAT-Clustering-PowerShell, WAS-Process-Model, Web-Asp-Net45, Web-Basic-Auth, Web-Client-Auth, Web-Digest-Auth, Web-Dir-Browsing, Web-Dyn-Compression, Web-Http-Errors, Web-Http-Logging, Web-Http-Redirect, Web-Http-Tracing, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Lgcy-Mgmt-Console, Web-Metabase, Web-Mgmt-Console, Web-Mgmt-Service, Web-Net-Ext45, Web-Request-Monitor, Web-Server, Web-Stat-Compression, Web-Static-Content, Web-Windows-Auth, Web-WMI, Windows-Identity-Foundation, RSAT-ADDS
-    write-host "Installed Windows Features"
+    write-host "Installed Windows Features" -ForegroundColor Green
+    Write-Host "Will Reboot in 8 Sec." -ForegroundColor Red -BackgroundColor Black
     sleep 8
     Restart-Computer
 }
 
 function Prepare-Exchange{
-    param ([string]$Directory)
+    param (
+        [string][Parameter(Mandatory=$true, Position=0)][ValidateScript({test-path $_})]$Directory
+    )
+    Set-Location $Directory
     $mountResult = Mount-DiskImage C:\ExchFiles\ExchangeServer2016-x64-cu11.iso -PassThru
     $DriveLetter = ($mountResult | Get-Volume).DriveLetter
     $DriveLetter+=':\'
@@ -85,6 +98,10 @@ function Prepare-Exchange{
 }
 
 function Install-Exchange{
+    param (
+        [string][Parameter(Mandatory=$true, Position=0)][ValidateScript({test-path $_})]$Directory
+    )
+    set-Location $Directory
     $mountResult = Mount-DiskImage C:\ExchFiles\ExchangeServer2016-x64-cu11.iso -PassThru
     $DriveLetter = ($mountResult | Get-Volume).DriveLetter
     $DriveLetter+=':\'
@@ -93,6 +110,7 @@ function Install-Exchange{
     $myProcess = Start-Process $myFile -ArgumentList "/IAcceptExchangeServerLicenseTerms","/Mode:Install","/Role:Mailbox" -PassThru
         $myProcess | Wait-Process
     #Restart-Computer
+    Pop-Location
 }
 
 function Move-Database{
